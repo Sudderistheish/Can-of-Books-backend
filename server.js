9a9c255a6b810bef6f4df6fc2bcd789e68ac2718
Justin Agim <justinagim@gmail.com>
2023-08-22 18:34:02 -0500

    bug correction

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.get('/', (request, response) => {
 app.get('/book', async (request, response) => {
     try {
         if (request.query.name) {
-            const book = await Book1.find({ name: request.query.name })
+            const book = await Book1.find({ name: request.query.name });
             response.send([book]);
         } else {
             const book = await Book1.find({});
-            if (Object.keys(book).length === 0) response.send([])
+            if (Object.keys(book).length === 0) response.send([]);
             response.send(book);
         }
 
